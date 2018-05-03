44981a61d020c0b6cf1d30d07b48ef3877e1c8a8
Praveen0510 <praveen.t.developer@gmail.com>
2018-05-03 18:50:42 +0530

    Modified

--- Build/build.bat
+++ Build/build.bat
@@ -1,3 +1,4 @@
+@echo off
 echo Build started
 “C:\jenkins\workspace\TestAutomation1\Installer\python-3.6.5.exe” /quiet InstallAllUsers=1 PrependPath=1 Include_test=0 TargetDir=c:\Python
 echo Python Installed
