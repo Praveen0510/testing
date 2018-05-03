e94c5d68bd415a3a679651953ef92541a6fbc2ad
Praveen0510 <praveen.t.developer@gmail.com>
2018-05-03 17:19:23 +0530

    Message added

--- Build/build.bat
+++ Build/build.bat
@@ -1,4 +1,6 @@
+echo Build started
 “C:\jenkins\workspace\TestAutomation1\Installer\python-3.6.5.exe” /quiet InstallAllUsers=1 PrependPath=1 Include_test=0 TargetDir=c:\Python
+echo Python Installed
 
 set test=C:\Users\username\AppData\Local\Programs\Python\Python35-32\Lib\site-packages
 
@@ -21,6 +23,7 @@ if not exist %test%\SikuliLibrary  pip install robotframework-SikuliLibrary
 
 if not exist %test%\robotremoteserver.py  pip install robotremoteserver
 
+echo Build completed
 
 exit
                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                 
